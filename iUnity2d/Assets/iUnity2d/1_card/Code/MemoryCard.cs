346d355bb68e74b5142e28fddf145cba2ca34949
DESKTOP-U98JLE6\12034 <1203448951@qq.com>
2018-10-10 17:25:24 +0800

    spawn cards &filp card

---
+++ iUnity2d/Assets/iUnity2d/1_card/Code/MemoryCard.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MemoryCard : MonoBehaviour {
+    //[SerializeField] private GameObject cardback;
+    private GameObject cardback;
+    [SerializeField] private SceneController controller;
+
+    private int _id;
+    public int id { get { return _id; } }
+
+    public void setCard(int id , Sprite sprite)
+    {
+        _id = id;
+        GetComponent<SpriteRenderer>().sprite = sprite;
+    }
+
+	// Use this for initialization
+	void Start () {
+        cardback = transform.Find("cardback").gameObject;   //find child
+        GameObject.Find("car");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnMouseDown()
+    {
+        if (cardback.activeSelf)
+        {
+            cardback.SetActive(false);
+        }
+    }
+}
---
+++ iUnity2d/Assets/iUnity2d/1_card/Code/SceneController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneController : MonoBehaviour {
+    [SerializeField] private MemoryCard originalCard;
+    [SerializeField] private Sprite[] CardSprite;
+
+
+    public  int gridRows = 2;
+    public  int gridCols = 4;
+    public  float offsetX = 2f;
+    public  float offsetY = 2.5f;
+
+
+    // Use this for initialization
+    void Start () {
+
+        Vector3 startPos = originalCard.transform.position;
+
+        for (int y =0;y<gridCols;y++)
+        {
+            for (int x=0;x<gridRows;x++)
+            {
+                MemoryCard card;
+                if (y==0&&x==0)
+                {
+                    card = originalCard;
+                }
+                else
+                {
+                    card = Instantiate(originalCard) as MemoryCard;
+                }
+
+                int id = Random.Range(0, CardSprite.Length);
+                card.setCard(id, CardSprite[id]);
+
+                float posX = x * offsetX + startPos.x;
+                float posY = y * offsetY + startPos.y;
+                card.transform.position = new Vector3(posX, posY, startPos.z);
+            }
+        }
+
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
