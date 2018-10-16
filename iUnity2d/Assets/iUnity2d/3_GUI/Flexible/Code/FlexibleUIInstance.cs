5a64e84468823deaee163befb1f241d859c40ece
DESKTOP-U98JLE6\12034 <1203448951@qq.com>
2018-10-16 17:56:51 +0800

    Create FlexibleButton Create Shortcut

---
+++ iUnity2d/Assets/iUnity2d/3_GUI/Flexible/Code/FlexibleUIInstance.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class FlexibleUIInstance : Editor {
+
+    [MenuItem("GameObject/Flexible UI/Button",priority =0)]
+    public static void Flexible_Button()
+    {
+        Create("FlexibleUI/FlexibleButton","Button");
+    }
+
+    static GameObject clickedObject;
+
+    private static GameObject Create(string path,string objectName) //objectName is now not use
+    {
+        GameObject instance = Instantiate(Resources.Load<GameObject>(path));
+        //instance.name = objectName;
+        clickedObject = UnityEditor.Selection.activeObject as GameObject;
+        if (clickedObject != null)
+        {
+            instance.transform.SetParent(clickedObject.transform, false);
+        }
+        return instance;
+    }
+
+}
