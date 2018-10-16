3371ddee0dc4a3351b7af9c48a130fdc36af1aa9
DESKTOP-U98JLE6\12034 <1203448951@qq.com>
2018-10-16 16:53:48 +0800

    ui_ScriptableObject UI & FlexibleUI & FlexibleUIButton

---
+++ iUnity2d/Assets/iUnity2d/3_GUI/Flexible/Code/FlexibleUIButton.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+[RequireComponent(typeof(Button))]
+[RequireComponent(typeof(Image))]
+public class FlexibleUIButton : FlexibleUI {
+//     public enum ButtonTypes
+//     {
+//         Default,
+//         Confirm,
+//         Decline,
+//         Warning
+//     }
+
+    Image _im;
+    Image _Icon;
+    Button _bu;
+// 
+//     public ButtonTypes buttonType;
+
+    protected override void OnSkinUI()
+    {
+        base.OnSkinUI();
+
+        _im = GetComponent<Image>();
+        //_Icon = transform.Find("Icon").GetComponent<Image>();
+        _bu = GetComponent<Button>();
+
+        _bu.transition = Selectable.Transition.SpriteSwap;
+        _bu.targetGraphic = _im;
+
+        _im.sprite = skinData.ButtonSprite;
+        _im.color = skinData.SpriteColorMutliply;
+        _bu.spriteState = skinData.ButtonSpriteState;
+        Debug.Log("update");
+
+// 
+//         switch (buttonType)
+//         {
+//             case ButtonTypes.Confirm:
+//                 _im.color = skinData.confirmColor;
+//                 _Icon.sprite = skinData.confirmIcon;
+//                 break;
+//             case ButtonTypes.Decline:
+//                 _im.color = skinData.declineColor;
+//                 _Icon.sprite = skinData.declineIcon;
+//                 break;
+//             case ButtonTypes.Default:
+//                 _im.color = skinData.DefaultColor;
+//                 _Icon.sprite = skinData.DefaultIcon;
+//                 break;
+//             case ButtonTypes.Warning:
+//                 _im.color = skinData.warningColor;
+//                 _Icon.sprite = skinData.warningIcon;
+//                 break; 
+//         }
+    }
+
+}
---
+++ iUnity2d/Assets/iUnity2d/3_GUI/Flexible/Code/FlexibleUIData.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+[CreateAssetMenu(menuName = "Flexible UI Data")]
+public class FlexibleUIData : ScriptableObject
+{
+    public Sprite ButtonSprite;
+    public SpriteState ButtonSpriteState;
+    public Color SpriteColorMutliply;
+
+//     public Color DefaultColor;
+//     public Sprite DefaultIcon;
+// 
+//     public Color confirmColor;
+//     public Sprite confirmIcon;
+// 
+//     public Color declineColor;
+//     public Sprite declineIcon;
+// 
+//     public Color warningColor;
+//     public Sprite warningIcon;
+}
